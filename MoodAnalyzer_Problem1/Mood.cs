f3367f726919b5a316d15d538c6f93a17044685d
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-12-03 15:48:06 +0530

    Added file

--- MoodAnalyzer_Problem1/Mood.cs
+++ MoodAnalyzer_Problem1/Mood.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using MoodAnalyzer_Problem1;
+using System.Security.Cryptography.X509Certificates;
+
 namespace MoodAnalyzer_Problem1
 {
     public class Mood
     {
-        string message=string.Empty;
+        public string message { get; set; }
         public Mood()
         {
 
@@ -21,6 +24,15 @@ namespace MoodAnalyzer_Problem1
         {
             try
             {
+                if(message==null)
+                {
+                    throw new MoodAnalysisExceptions(MoodError.Null.ToString());
+                }
+                if (message == " ")
+                {
+                    throw new MoodAnalysisExceptions(MoodError.Empty.ToString());
+                }
+
                 bool result1 = message.Contains("Happy", StringComparison.OrdinalIgnoreCase);
                 if (result1)
                 {
@@ -36,6 +48,10 @@ namespace MoodAnalyzer_Problem1
                     return "Happy";
                 }
             }
+            catch(MoodAnalysisExceptions ex)
+            {
+                return ex.Message;
+            }
             catch (Exception ex)
             {
                 return "Happy";
@@ -43,9 +59,53 @@ namespace MoodAnalyzer_Problem1
         }
     }
     public enum MoodError
-    {
-        Invalid,
+    { 
         Null,
-        Empty
+        Empty,
+        No_Such_Class,
+        No_Such_Method,
+
+    }
+    public class MoodFactory
+    {
+        public MoodFactory()
+        {
+
+
+        }
+        public static object CreateInstance(string className, [Optional] string constructorName)
+        {
+
+            try
+            {
+                Type type = Type.GetType(className);
+                if (type == null)
+                {
+                    throw new MoodAnalysisExceptions(MoodError.No_Such_Class.ToString());
+                }
+                else if (constructorName != null)
+                {
+                    bool exists = false;
+                    var constructors = type.GetConstructors();
+                    foreach (var constructor in constructors)
+                    {
+                        if (constructor.ToString() == constructorName)
+                        {
+                            exists = true;
+                            continue;
+                        }
+                    }
+                    if (!exists)
+                    {
+                        throw new MoodAnalysisExceptions(MoodError.No_Such_Method.ToString());
+                    }
+                }
+                return type;
+            }
+            catch (MoodAnalysisExceptions ex)
+            {
+                return ex.Message;
+            }
+        }
     }
 }
